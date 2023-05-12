20746851c5efffb44959b7fb787f3e423c4d71e2
dionkb <dionkbaskara@gmail.com>
2023-05-11 21:28:47 -0400

    set up basic reponse paths with no endpoint currently

--- server.js
+++ server.js
@@ -65,6 +65,53 @@ function init() {
 init()
 .then(response => {
     // Used for testing purposes, will delete when no longer needed
-    console.log(response)
+    console.log(response);
+    if (response.mainPrompt === "View All Departments") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "View All Roles") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "View All Employees") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Add A Department") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Add A Role") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Add An Employee") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Update An Employee Role") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Update Employee Managers") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "View Employees (By Manager)") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "View Employees (By Department)") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Delete Departments") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Delete Roles") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "Delete Employees") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "View Total Budget") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else if (response.mainPrompt === "View Budget (By Department)") {
+        console.log("You selected: " + response.mainPrompt)
+    }
+    else {
+        console.log(err);
+    }
 });
-
