66b8616f6e3207fede8b90a8139d1a02e2d84075
dionkb <dionkbaskara@gmail.com>
2023-05-13 22:38:44 -0400

    see comment

--- db/schema.sql
+++ db/schema.sql
@@ -23,9 +23,6 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    -- Don't forget, the following will link to the manager of this employee, can be null if they have no manager
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    role_id INT REFERENCES role(id),
+    manager_id INT REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,15 +9,25 @@ VALUES ("History"),
        ("Business");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Instructor", 60000, 1),
-       ("TA", 40000, 3),
-       ("SSA", 30000, 6),
-       ("Advisor", 35000, 7);
+VALUES ("History Instructor", 60000, 1),
+       ("Science Instructor", 60000, 3),
+       ("Business Instructor", 60000, 7),
+       ("Math TA", 40000, 2),
+       ("Game Design TA", 40000, 6),
+       ("English TA", 40000, 4),
+       ("Art Advisor", 35000, 5),
+       ("History Advisor", 35000, 1),
+       ("Science Advisor", 35000, 3),
+       ("Business Advisor", 35000, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("D", "B", 1, NULL),
-       ("R", "B", 2, NULL),
-       ("N", "H", 4, NULL),
-       ("M", "L", 3, NULL),
-       ("D", "K", 4, NULL),
-       ("A", "J", 4, NULL);
+VALUES ("Richie", "Holmes", 1, NULL),
+       ("Hattie", "Jacobson", 8, 1),
+       ("Christina", "Rowe", 2, NULL),
+       ("Nathan", "Vasquez", 9, 3),
+       ("Kyron", "Edwards", 3, NULL),
+       ("Cecily", "Day", 4, NULL),
+       ("Nevaeh", "Newton", 5, NULL),
+       ("Soraya", "Castillo", 6, NULL),
+       ("Jerry", "Anderson", 7, NULL),
+       ("Gareth", "Barron", 10, 5);
